08be738d0c2ee2053d55d6f8cbc2453a82e2c07c
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-11 21:03:19 -0300

    Usando novas funções

--- JavaScript/Brain.js
+++ JavaScript/Brain.js
@@ -1,4 +1,4 @@
-function x(){
+/*function x(){
     console.log("Caiu na funcao")
 }
 var nun = 1;
@@ -12,7 +12,7 @@ if (!nun > 0){
 //ft.push / unshift = ADICIONA
 //ft.pop / shift = REMOVE
 
-/*var arrayteste = ['Matias','carlos','Pedro','Henrique','Igor'];
+var arrayteste = ['Matias','carlos','Pedro','Henrique','Igor'];
 console.log(arrayteste[1]);
 arrayteste.unshift('Pedro')
 console.log(arrayteste[0]);
@@ -28,32 +28,22 @@ var Array2 = ['marcos','julia','Luan']
 var array3 = array1.concat(Array2);
 console.log(array3);*/
 
+//TESTANDO LOGICAS...
+
 var ar = [1,2,3,4];
 console.log(ar);
 
-var arr = [1,2,3,4,5,6,7,8,9,10];
-for (var i = 0; i < arr.length; i++){
-    if (arr[i]) {
-        console.log(arr[i]);
+var arr1 = [1,2,4,10,15,18];
+for (var i = 0; i < 5; i++){
+    if (arr1[i] % 2 == 0) {
+       arr1[1] = arr1[i] + 1;
+    }else if (arr1[i] % 2 !=0){
+        console.log('Impar');
     }
 }
+console.log(arr1);
 
-var arr = [2,4,6,8,10,12];
-for (var i = 0; i < arr.length; i++){
-    if (arr[i] % 2 == 0) {
-        arr[i] = arr[i] + 1;
-        console.log(arr);
-    }
-}
 
-var arr = [1,3,5,7,9,11];
-for (var i = 0; i < arr.length; i++){
-    if (arr[i] % 2 != 0) {
-        arr.splice(i, 1);
-        i--;
-        console.log(arr);
-    }
-}
 
 
 
